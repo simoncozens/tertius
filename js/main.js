8bd7b552a15330cbd7e2bffb977e1448a409a14f
Simon Cozens <simon@simon-cozens.org>
2013-07-14 22:50:12 +0900

    Use the new reference parser and efficiently suck up parsed references.

--- js/main.js
+++ js/main.js
@@ -11,36 +11,50 @@ Tertius = {
     });
 	},
   search: function() {
-    var ref = $("#searchbar").val().split(/ /);
+    var ref = $("#searchbar").val();
     $("#bible").empty();
-    var currentBibles = [ Tertius.Bibles.net, Tertius.Bibles.shinkyodo ];
+    this.currentBibles = [ Tertius.Bibles.net, Tertius.Bibles.shinkyodo ];
     var iterators = [];
+    var bibleRef = BibleRefParser(ref);
     var head= $('<tr class="bibleheader"><td/></tr>');
-    currentBibles.forEach(function (b) {
-      iterators.push(b.lookup(ref[0], ref[1],ref[2]));
-      head.append("<th>"+b.documentElement.getAttribute("name")+"</th>");
+    this.currentBibles.forEach(function (b) {
+      head.append("<th>"+b.name+"</th>");
     });
     $("#bible").append(head);
-    var nonempty = iterators.length;
-    while (nonempty > 0) {
-      var verses = [];
-      var nonempty = 0;
-      iterators.forEach(function(i) {
-        var verse;
-        if (verse = i.iterateNext()) {
-          verses.push(verse);
-          nonempty++;
-        }
-      });
-      if (verses.length > 0) {
-        var row = $("<tr><th>"+verses[0].getAttribute("num")+"</th></tr>");
-        verses.forEach(function (v) {
-          var td = $("<td>"); td.append(v.textContent);        
-          row.append(td);
-        });
-        $("#bible").append(row);
-      }
+    if (bibleRef) {
+      return this.showBible(bibleRef);
     }
+    // It's a word search
+  },
+  showBible: function(ref) {
+    /* For efficient searching we are going to get all the verse information 
+    for each passage from a given source in one go, then fit it together later, 
+    rather than iterating each verse in each source. */
+    var results= {};
+    var order = [];
+    ref.references.forEach( function(r) {
+      var thisref;
+      Tertius.currentBibles.forEach(function (b) {
+        b.lookup(r.bookId, r.chapter, r.startVerse, r.endVerse).forEach(function (verse) {
+          if (!results[verse.reference]) {
+            results[verse.reference] = {};
+            order.push(verse.reference);
+          }
+          results[verse.reference][b.name] = verse.text;
+        });
+      });
+    });
+    order.forEach( function(r) {
+      var row = $('<tr><th class="ref">'+r+"</th></tr>");
+      console.log(results[r]);
+      Tertius.currentBibles.forEach(function (b) {
+        v = results[r][b.name];
+        var td = $("<td>"); td.append(v);
+        row.append(td);
+      });
+      $("#bible").append(row);
+
+    });
   },
 
 };
--- js/tertius/biblesources/xml.js
+++ js/tertius/biblesources/xml.js
@@ -6,13 +6,18 @@ Tertius.BibleSources.xml = {
       error: function (j, text, error) { console.log(error); }
     }).done(function (bible) {
       $.extend(bible, Tertius.BibleSources.xml);
+      bible.name = bible.documentElement.getAttribute("name");
       Tertius.Bibles[name] = bible;
     });
   },
 
-  lookup: function(book, chapter, verse) {
-    var versit = this.evaluate("//bible/book[@num="+book+"]/chapter[@num="+chapter+"]/verse", this, null, 0, null);
-    return versit;
+  lookup: function(book, chapter, v1, v2) {
+    var versit = this.evaluate("//bible/book[@num="+book+"]/chapter[@num="+chapter+"]/verse[@num >= "+v1+" and @num <= "+v2+"]", this, null, 0, null);
+    var results = [];
+    while (verse = versit.iterateNext()) {
+      results.push({ reference: chapter+":"+verse.getAttribute("num"), text: verse.textContent });
+    }
+    return results;
   },
 
   search: function() {
