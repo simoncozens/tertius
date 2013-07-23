bd7819eac8fbec9629c3b8f168e0267f0a7e4b6a
Simon Cozens <simon@simon-cozens.org>
2013-07-23 23:32:18 +0900

    Implement both FTS and non-FTS searching.

--- js/tertius/biblesources/sql.js
+++ js/tertius/biblesources/sql.js
@@ -2,22 +2,27 @@ Tertius.BibleSources.sql = {
   load: function (name,cb) {
     var bible = window.sqlitePlugin.openDatabase({name: name});
     $.extend(bible, Tertius.BibleSources.sql);
-    var name;
     bible.transaction(function(tx) {
-      tx.executeSql("select name from metadata", [], function(tx,res) {
+      var options = {};
+      tx.executeSql("select * from metadata", [], function(tx,res) {
         name = res.rows.item(0).name;
-        bible.name = name;
-        Tertius.Bibles[name] = bible;
+        var len = res.rows.length, i;
+        for (i = 0; i < len; i++) {
+          var row = res.rows.item(i);
+          options[row.k] = row.v;
+        }
+        bible.options = options;
+        bible.name = options.abbrev;
+        Tertius.Bibles[options.abbrev] = bible;
         cb();
       });
     });
   },
-  lookup: function(book, chapter, v1, v2, cb) {
+  _sql_search: function(query, params, cb) {
     var results = [];
     var that = this;
     this.transaction(function(tx) {
-      tx.executeSql("select * from bible where book=? and chapter=? and verse >= ? and verse <= ?",
-        [book, chapter, v1, v2], function(tx, res) {
+      tx.executeSql(query, params, function(tx, res) {
         var len = res.rows.length, i;
         for (i = 0; i < len; i++) {
           var row = res.rows.item(i);
@@ -27,8 +32,16 @@ Tertius.BibleSources.sql = {
       });
     });
   },
-
-  search: function() {
-
+  lookup: function(book, chapter, v1, v2, cb) {
+    this._sql_search("select * from bible where book=? and chapter=? and verse >= ? and verse <= ?", [book, chapter, v1,v2],cb);
+  },
+  search: function(text,cb) {
+    var query;
+    if (this.options.fts) {
+      query = "SELECT bible_fts.book, bible_fts.chapter, bible_fts.verse, bible.content FROM bible_fts, bible WHERE bible_fts.content MATCH ? AND bible_fts.book = bible.book AND bible_fts.chapter=bible.chapter AND bible_fts.verse = bible.verse";
+    } else {
+      query = "SELECT book, chapter, verse FROM bible where content LIKE ?";
+    }
+    this._sql_search(query, [text], cb);
   }
 };
