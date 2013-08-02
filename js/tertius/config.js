63f544fb0e1a0a6d29328f4b144a37a4ec70ea9b
Simon Cozens <simon@simon-cozens.org>
2013-08-02 09:36:54 +0900

    Config file for web version.

---
+++ js/tertius/config.js
@@ -0,0 +1,7 @@
+Tertius.config = {
+	loader: "xml",
+	bibles: [ "LEB", "kjv", "sbl", "wlc" ],
+	UI: Tertius.UIs.JQM,
+    DataStorage: Tertius.DataStorageMethods.HTML5LocalStorage,
+	start: ["John", 3]
+};
