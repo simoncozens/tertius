73ed59eb5f53e5a9fd74c0f0c778a6a9169d9693
Simon Cozens <simon@simon-cozens.org>
2013-08-02 15:55:41 +0900

    Further protection against breakage.

--- js/tertius/settings.js
+++ js/tertius/settings.js
@@ -1,10 +1,13 @@
 Tertius.SettingsManager = {
-  settings: {
+  defaultSettings: {
     fontSize: "100",
     ruby: "1"
   },
   load: function(cb) {
-    Tertius.DataStorage.loadJSON("settings", Tertius.SettingsManager, cb);
+    Tertius.DataStorage.loadJSON("settings", Tertius.SettingsManager, function() {
+      if (!Tertius.SettingsManager.settings) Tertius.SettingsManager.settings = Tertius.SettingsManager.defaultSettings;
+      cb();
+    });
   },
   save: function() {
     Tertius.DataStorage.saveJSON("settings", Tertius.SettingsManager.settings);
